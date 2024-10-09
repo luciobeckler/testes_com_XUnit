ca9196e82448f9b320c396c4cba8786a98b436f5
Lúcio Beckler <luciobecklerp@gmail.com>
2024-10-09 11:05:54 -0300

    feat: adicionando testes de erros de rota null e preco negativo

--- tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -17,5 +17,31 @@ namespace JornadaMilhas.Test
 
             Assert.Equal(validacao, oferta.EhValido);
         }
+
+        [Fact]
+        public void TestandoErro()
+        {
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100;
+
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+        }
+
+        [Fact]
+        public void TestandoPrecoNegativo()
+        {
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = -100;
+
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+        }
     }
 }
